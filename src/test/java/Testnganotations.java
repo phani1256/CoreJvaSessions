264d5ec8a5c7667379dbb584dbd89d20da191e7d
Vemuri Naveen <vemurinaveen069@gmail.com>
2022-07-25 09:09:25 +0530

    Testng

---
+++ src/test/java/Testnganotations.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class Testnganotations {
+    @Test
+    public void add(){
+        System.out.println("addition");
+    }
+    @Test
+    public void sub(){
+        System.out.println("Subtrction output");
+    }
+}
