0f22db5e7ab2e26510e6c337d915f7335c6cc920
Vemuri Naveen <vemurinaveen069@gmail.com>
2022-07-25 08:38:55 +0530

    Encapsulation

---
+++ src/OOPSEncapsulation/main.java
@@ -0,0 +1,9 @@
+package OOPSEncapsulation;
+
+public class main {
+    public static void main(String[] args) {
+        EncapsulationEx obj=new EncapsulationEx();
+        obj.setName("Phani");
+        System.out.println(obj.getName());
+    }
+}
