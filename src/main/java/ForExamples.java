daf243f8ad3528eac708a49ad5fdd2fdf6b11211
Vemuri Naveen <vemurinaveen069@gmail.com>
2022-08-03 09:15:20 +0530

    forloop

---
+++ src/main/java/ForExamples.java
@@ -0,0 +1,52 @@
+public class ForExamples {
+    public static void main(String[] args) {
+       /* //sum of 10 numbers
+        int sum=0;
+        for (int i=1;i<=10;i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+        //Average of 10 numbers
+        double average=(double)sum/10;
+        System.out.println(average);*/
+        //Sum of elements in the array
+        /*int[] a={1,2,3,4,5,6,7,8,9,10};
+        int sum=0;
+        for(int i=0;i<=a.length-1;i++){
+            sum=sum+a[i];
+        }
+        System.out.println(sum);*/
+        //Swapping of two numbers with third variable
+        /*int a=10;
+        int b=20;
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);*/
+       /* //Swapping of 2 numbers without third variable
+        int a=10;
+        int b=20;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);*/
+        //Pattern Program
+        /*for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+        /*String[] names={"Manoj","Purna","Srinu","phani"};
+        for(int i=0;i<=names.length-1;i++){
+            System.out.println(names[i]);
+            if(names[i].equals("Purna")){
+                System.out.println("purna is available");
+                break;
+            }
+        }*/
+    }
+}
